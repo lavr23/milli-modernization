3681d0497db0a70ec137601aa94ae19dff32ea79
Vladimir Lavrikov <vlavrikov83@gmail.com>
2021-11-28 00:56:36 +0300

    bonus program

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        int amountTickets = 2100;
+        int milesProgram = 20;
+        int bonus = amountTickets / milesProgram;
+        System.out.println("Your bonus is: " + bonus + " miles");
 
     }
 }
+
